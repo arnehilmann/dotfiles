2296b2ba668f9844682442ae877e452d59aca001
Arne Hilmann <arne@hilmann.de>
2015-08-24 16:18:23 +0200

    simplify color handling

--- _vimrc.py
+++ _vimrc.py
@@ -21,75 +21,11 @@ def toggle_snake_case_camel_case():
         snake_case = "_".join([chunk.lower() for chunk in chunks])
         replace_word(snake_case)
 
-colors = {"normal": """
-1 0
-2 0
-3 0
-4 0
-5 0
-6 0
-7 0
-""",
-    "atom": """
-6 1
-7 1
-4 3
-7 3
-7 4
-7 5
-7 6
-"""}
 
-max_nr = {"normal": 0, "atom": 0}
-for tokentype, lines in colors.items():
-    for line in lines.splitlines():
-        if not line:
-            continue
-        if line.startswith("#"):
-            continue
-        fg, bg = line.split()
-        command("hi! def _semhl%s%s ctermfg=%s ctermbg=%s" % (tokentype, max_nr[tokentype], fg, bg))
-        max_nr[tokentype] = max_nr[tokentype] + 1
+
+
+from snake.plugins import vim_cuttlefish as cuttlefish
 
 @key_map("<leader>S")
-def toggle_semantic_highlight():
-    buffer = get_current_buffer()
-    lines = []
-
-    blacklist = set(["defmodule", "do", "end", "def", "use"])
-    command("syn keyword _ %s" % " ".join(blacklist))
-    command("syn match comment \"\v#.*$\"")
-    command("hi! link comment Comment")
-
-    tokencount = {}
-    for line in get_buffer_lines(buffer):
-        if line.startswith("#"):
-            continue
-        line = re.sub("[ \.\t,;()\[\]{}=\"\'\|]+", " ", line).strip()
-        for token in line.split():
-            token = token.strip()
-            if not token:
-                continue
-            if token in blacklist:
-                continue
-            if token.isdigit():
-                continue
-            tokencount[token] = tokencount.get(token, 0) + 1
-
-    token2color = {}
-    current = {"normal": 0, "atom": 0}
-    for token, count in tokencount.items():
-        if count <= 1:
-            continue
-        if token.startswith(":"):
-            tokentype = "atom"
-        else:
-            tokentype = "normal"
-        token2color[token] = "_semhl%s%s" % (tokentype, current[tokentype])
-        current[tokentype] = (current[tokentype] + 1) % max_nr[tokentype]
-
-    for token, color in token2color.items():
-        command("syn keyword %s %s" % (color, token))
-        lines.append("syn keyword %s %s" % (color, token))
-
-    set_buffer_lines(buffer, lines)
+def toggle_gensemhl():
+    cuttlefish.toggle_semantic_highlight()
